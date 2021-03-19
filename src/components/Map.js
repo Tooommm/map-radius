94c8bca9d3b2c1fa6aa94e1bce8cd0fb8c619fc3
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-03-19 21:18:42 +0100

    mapbox installed

---
+++ src/components/Map.js
@@ -0,0 +1,31 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import React, { Component } from "react";
+import ReactMapboxGl, { Feature, Layer } from "react-mapbox-gl";
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API_TOKEN;
+console.log(MAPBOX_TOKEN);
+
+const MapBox = ReactMapboxGl({
+  accessToken: MAPBOX_TOKEN,
+});
+
+class Map extends Component {
+  render() {
+    return (
+      <MapBox
+        style="mapbox://styles/mapbox/light-v10"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </MapBox>
+    );
+  }
+}
+
+export default Map;
